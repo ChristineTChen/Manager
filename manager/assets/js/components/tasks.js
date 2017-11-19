72f356fd20a9b343eb4588ab637923da91f1fcb8
christinetchen <chen.chr@husky.neu.edu>
2017-11-19 18:39:39 -0500

    Added ability to create new tasks

--- manager/assets/js/components/tasks.js
+++ manager/assets/js/components/tasks.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import {PageHeader, Grid, Row, Col, Thumbnail, Button} from 'react-bootstrap';
-
-
+import {PageHeader, Grid, Row, Col, Thumbnail, Button, Form, FormGroup, ControlLabel, FormControl} from 'react-bootstrap';
+import moment from 'moment';
 class Tasks extends React.Component {
 
 	constructor(props) {
@@ -12,6 +11,10 @@ class Tasks extends React.Component {
 
 			tasks: []
 		}
+
+		this.newTaskEle = null;
+
+		this.createTask = this.createTask.bind(this)
 
 		this.loadTasks();
 	}
@@ -42,14 +45,42 @@ class Tasks extends React.Component {
 	  	});
 	}
 
+	createTask() {
+		let data = {task: {
+			manager_id: 2,
+			assignee_id: window.current_user_id,
+			desc: this.newTaskEle.value,
+		}};
+		$.ajax({
+			url: '/api/v1/tasks',
+			dataType: "json",
+      		contentType: "application/json",
+			data:  JSON.stringify(data),
+			method: "POST",
+			success: this.loadTasks,
+		})
+	}
+
 	render() {
 	if (!window.user_name){
 		return null;
 	}
+	console.log('render')
 
       return (
       	<div>
             <PageHeader>Bulletin Board</PageHeader>
+			 
+
+			 <Form inline>
+			    <FormGroup controlId="formInlineName">
+			      <ControlLabel>New Task</ControlLabel>
+			      <FormControl inputRef = {(element) => {this.newTaskEle = element}} type="text" placeholder="Task description" />
+			    </FormGroup>
+			    <Button onClick={this.createTask} >
+			      Post Task
+			    </Button>
+			  </Form>
 
             <Grid>
 			    <Row>
@@ -57,7 +88,7 @@ class Tasks extends React.Component {
 			    	return (
 			    		<Col xs={6} md={4} key={index + task.desc}>
 					        <Thumbnail>
-					          <h3>Task {index + 1}</h3>
+					          <h3>Task #{task.id}</h3>
 					          <p>Description: {task.desc}</p>
 					          <p>Date posted: {task.inserted_at}</p>
 					          <p>
