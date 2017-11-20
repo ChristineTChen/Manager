eceb9d75cf3af54ddbb5c7b36b157cf6e6454de7
christinetchen <chen.chr@husky.neu.edu>
2017-11-19 19:27:25 -0500

    Show user list

--- manager/lib/manager_web/controllers/teams_controller.ex
+++ manager/lib/manager_web/controllers/teams_controller.ex
@@ -1,7 +1,18 @@
 defmodule ManagerWeb.TeamsController do 
 	use ManagerWeb, :controller 
 
+	alias Manager.Accounts.User
+	alias Manager.Accounts
+
 	def index(conn, _params) do 
-		render conn, "index.html"
+		users = Accounts.list_users()
+		current_user = conn.assigns[:current_user]
+		if current_user do 
+			render(conn, "index.html", users: users)
+		else
+			conn
+			|> redirect(to: "/")
+			|> halt()
+		end
 	end
 end
